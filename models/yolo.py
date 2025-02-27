8652abaf45f42fad1e0e50f0a7aacdb6ab5777f3
Xindian Long <Xindian.Long@sas.com>
2025-02-27 09:27:05 -0500

    add self.vaidation so it is backward compatible

--- models/yolo.py
+++ models/yolo.py
@@ -45,6 +45,7 @@ class Detect(nn.Module):
         self.register_buffer('anchors', a)  # shape(nl,na,2)
         self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
         self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
+        self.validation = False
 
     def forward(self, x):
         # x = x.copy()  # for profiling
